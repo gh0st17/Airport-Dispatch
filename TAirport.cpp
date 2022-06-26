0425397c7e2853a90b18022c0b486c71f7f1167e
Степан <31482802+gh0st17@users.noreply.github.com>
2022-06-26 17:02:40 +0300

    Initial commit

--- TAirport.cpp
+++ TAirport.cpp
@@ -30,12 +30,12 @@ void TAirport::Do(float t0, float tk) {
     for (auto& la : LA) {
       pos = la->getPos();
       if (!la->getLanding() && LA_cur == LA_n) {
-        cout << "LA " << LA_n + 1 << "[landing]: x: " << pos[0] << " y : " << pos[1] << endl;
+        cout << "LA " << LA_n + 1 << '(' << setw(7) << pos[0] << ',' << setw(7) << pos[1] << ")[landing]\n";
         la->move(t, la->get_a(f, x, y, l));
         la->updateLanding(x, y, l, &LA_cur, LA_n);
       }
       else if (!la->getLanding()) {
-        cout << "LA " << LA_n + 1 << "[waiting]: x: " << pos[0] << " y : " << pos[1] << endl;
+        cout << "LA " << LA_n + 1 << '(' << setw(7) << pos[0] << ',' << setw(7) << pos[1] << ")[waiting]\n";
         if(dynamic_cast<TAircraft*>(la) != nullptr)
           la->move(t, la->get_a(f, x, y, l));
       }
